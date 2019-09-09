997ca9b56fc7a068a533ec0270cceda3efb491b1
Kenneth Spatta <kenyounot@gmail.com>
2019-09-09 18:08:38 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  if array.size == 2
+    array.join(' and ')
+  elsif array.size == 3
+    joined = array.join(', ')
+    joined_array = joined.split(" ")
+    joined_array.insert(-2, "and").join(" ")
+  elsif array.size > 3
+    joined = array.join(', ')
+    splitted = joined.split(',').insert(-2, "and").join(",").gsub("and,", " and")
+  else
+    array.join
+  end
 end
