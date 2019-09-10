07e78bd62c68710e47971ab31b3c68f2938dba36
Danny Buckley <dannyboy.msn@hotmail.com>
2019-09-10 12:27:34 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,22 @@
 def oxford_comma(array)
+#{}"#{array.join(", ")}"
+new_array = ""
 
+  if array.length == 1
+    new_array = "#{array[0]}"
+
+  elsif array.length == 2
+      new_array = "#{array[0]} and #{array[1]}"
+  else
+      array.each_with_index.collect do |element, index|
+        if index < array.length - 1
+          new_array << "#{element}, "
+
+        else
+          new_array << "and #{element}"
+        end
+      end
+  end
+
+new_array
 end
