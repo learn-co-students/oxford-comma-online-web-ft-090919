8087fa46aa50555bfcf44e7dc52bb88e9f0b8401
Maynard Hartman <mjhartman@brighthouse.com>
2019-09-12 17:36:02 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+ case array.length
+ when array.length == 3
+   result << array[0] + ',' + array[1] + ',' + array[2]
+ end
+ when array.length == 2
+   result << array[0] + ',' + array[1]
+ end
+ when array.length == 1
+   result << array[0]
+ end 
+ when array.length >= 3
+   result = array.join(',')
+ end 
+ return result
 end
