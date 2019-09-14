dc78b8d97748522ed579b5ef1bb76360b3d425d3
Lialah Barrer <lrbarrer@gmail.com>
2019-09-14 18:07:28 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  #If there is only one element, return it as a string
+  if array.size == 1
+    array.first
+  elsif array.size == 2
+    array.join(" and ")
+  else
+    array[-1] = "and " + array[-1]
+    array.join(", ")
+  end
 end
