cbb4ae4812e2a31e2aff2c6f78259e39037b2ad6
mmman64 <mcnultymarco@gmail.com>
2020-01-10 14:48:46 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
+  if array.size == 1
+    return array.first
+  elsif array.size == 2
+    return "#{array.first} and #{array.last}"
+  else
+    last_word = array.pop
+    sentence = ""
 
+    array.each do |str|
+      sentence << "#{str}, "
+    end
+
+    sentence << "and #{last_word}"
+  end
 end
